92a357c4a17e373dbac9c29a54426966fda52d0d
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-08-22 22:00:46 -0300

    Remove useless parameters

--- src/services/example.service.js
+++ src/services/example.service.js
@@ -1,4 +1,4 @@
-export async function example (provider, token) {
+export async function example () {
   const response = await window.fetch('https://jsonplaceholder.typicode.com/todos')
     .then(response => response.json()).catch(error => console.log(error))
 
