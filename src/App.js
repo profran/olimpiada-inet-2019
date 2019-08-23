03e80b1fa62739c6dbe986e557a53bff0eee68f8
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-08-22 21:59:31 -0300

    Replace animation library with react-spring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
-import Anime from 'react-anime'
 import { Grid, Card, CardContent, Typography } from '@material-ui/core'
 import { example } from './services/example.service'
+import { useSpring, animated } from 'react-spring'
+
+function FadeCard (props) {
+  const animation = useSpring({ opacity: 1, from: { opacity: 0 } })
+  return <animated.div style={animation}>{props.children}</animated.div>
+}
 
 export default class App extends Component {
   constructor (props) {
@@ -17,11 +22,6 @@ export default class App extends Component {
   }
 
   render () {
-    const animeProps = {
-      translateY: [-64, 0],
-      delay: (el, i) => i * 50
-    }
-
     const { todos } = this.state
 
     console.log(JSON.stringify(todos, 2, null))
@@ -29,9 +29,9 @@ export default class App extends Component {
     return (
       <Grid container justify='center'>
         <Grid item container sm={6} spacing={2}>
-          <Anime {...animeProps}>
-            {todos.map((v, i) => (
-              <Grid key={i} item xs={12} sm={6} lg={4}>
+          {todos.map((v, i) => (
+            <Grid key={i} item xs={12} sm={6} lg={4}>
+              <FadeCard>
                 <Card>
                   <CardContent>
                     <Typography variant='h6'>
@@ -39,9 +39,9 @@ export default class App extends Component {
                     </Typography>
                   </CardContent>
                 </Card>
-              </Grid>
-            ))}
-          </Anime>
+              </FadeCard>
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     )
